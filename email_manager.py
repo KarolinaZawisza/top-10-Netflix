b4037750892641601dc93bd60f50973b242331f3
KarolinaZawisza <zawiszakarolina.t@gmail.com>
2021-09-23 21:59:42 +0200

    Changing scraping system

--- email_manager.py
+++ email_manager.py
@@ -9,7 +9,7 @@ class EmailManager:
             connection.starttls()
             connection.login(user=MAIN_EMAIL, password=MAIN_EMAIL_PASSWORD)
             connection.sendmail(from_addr=MAIN_EMAIL,
-                                to_addrs=M_EMAIL,
+                                to_addrs=MAY_EMAIL,
                                 msg=message.encode('utf-8')
                                 )
             print(f'Send to {M_EMAIL} at {time}')
--- main.py
+++ main.py
@@ -3,32 +3,33 @@ from bs4 import BeautifulSoup
 import datetime as dt
 from email_manager import EmailManager
 
-# date = dt.datetime.now().strftime('%Y-%m-%d')
+date = dt.datetime.now().strftime('%Y-%m-%d')
 time = dt.datetime.now().strftime('%H:%M:%S')
-date = '2021-09-22'
+
 response = requests.get(f'https://flixpatrol.com/top10/netflix/poland/{str(date)}/').text
 soup = BeautifulSoup(response, 'html.parser')
+
+description_labels = [item.getText() for item in soup.find_all(name='h3', class_='table-th text-gray-400 text-center')]
+movie_index = int(description_labels.index('TOP 10 Overall'))
 
 titles = [item.getText() for item in soup.find_all(name='a', class_='hover:underline')]
 
 position = 0
 top_movies = []
-top_shows = []
+print(titles)
 
 for title in titles:
     position += 1
-    if 11 <= position < 21:
-        top_movies.append(f'{position-10}. {title}')
-    elif 21 <= position < 31:
-        top_shows.append(f'{position-20}. {title}')
+    if (movie_index*10)+1 <= position <= (movie_index*10)+10:
+        top_movies.append(f'{position}. {title}')
+
 
 movies_formatted = " \n".join(top_movies)
-shows_formatted = " \n".join(top_shows)
 
 message = f'SUBJECT: Top 10 Netflix ({date})\n\n' \
           f'Top 10 filmów:\n' \
           f'{movies_formatted}\n\n' \
-          f'Top 10 seriali: \n' \
-          f'{shows_formatted}'
+
+print(movies_formatted)
 
 EmailManager.send_email(message, time)
